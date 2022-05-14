ac68b2bc454c4ba62271f75fe581d7837d361b43
Stanislav Bozhimirov <bozhimirov@gmail.com>
2022-05-14 11:11:25 +0300

    solved

---
+++ Functions Advanced - Lab/04. Rectangle.py
@@ -0,0 +1,11 @@
+def rectangle(length, width):
+    def area():
+        return length * width
+
+    def perimeter():
+        return 2 * (length + width)
+
+    if not isinstance(length, int) or not isinstance(width, int):
+        return 'Enter valid values!'
+    return f'''Rectangle area: {area()}
+Rectangle perimeter: {perimeter()}'''
